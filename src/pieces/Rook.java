370da875e53c0f4826349ae10b4e286d85278400
adamfariello <adam.fariello@rutgers.edu>
2022-03-20 17:58:17 -0400

    Oh yeah
    B
    Updated: Piece to have a method to handle space legality
    Updated Rook to calculated all legal spaces it can be placed in its position
    Added rankFileConversion
    Also other stuff I can't remebmer

--- src/pieces/Rook.java
+++ src/pieces/Rook.java
@@ -47,6 +47,7 @@ public class Rook extends Piece {
 			} else {
 				//Friendly piece detected, can't move there or anywhere else
 				break;
+<<<<<<< HEAD
 			}
 		}
 		System.out.print("\n");
@@ -61,11 +62,49 @@ public class Rook extends Piece {
 					moves.add(temp);
 				}
 			} else { 
+=======
+			}
+		}
+		System.out.print("\n");
+		*/
+		for (int i = position[0] - 1; i > -1; i--) {
+			String temp = checkSpace(board, i, position[i]);
+			if (temp != null) {
+				if (Character.isUpperCase(temp.charAt(0)) == true) {
+					moves.add(temp.toLowerCase());
+					break;
+				} else {
+					moves.add(temp);
+				}
+			} else { 
+				break;
+			}
+		}
+		
+		//Going Down
+		//the "+ 1" is to skip checking the same space the piece it's on
+		for (int i = position[0] + 1; i < 8; i++) {
+			System.out.printf("Calculating (%d,%d) ", i, position[1]);
+			if (board[i][position[1]].getPiece() == null) {
+				//Blank Space, free spot
+				int [] temp = {i, position[1]};
+				moves.add(rankFileConversion.ArraytoRankFile(temp));
+			} else if (board[i][position[1]].getPiece() != null && 
+					   board[i][position[1]].getPiece().getPieceName().charAt(0) != 
+					   this.getPieceName().charAt(0)) {
+				//Space is an enemy spot, can take, but can't go beyond
+				int [] temp = {i, position[1]};
+				moves.add(rankFileConversion.ArraytoRankFile(temp));
+				break;
+			} else {
+				//Friendly piece detected, can't move there or anywhere else
+>>>>>>> 341b3af (Updated: Piece to have a method to handle space legality)
 				break;
 			}
 		}
 		System.out.print("\n");
 		
+<<<<<<< HEAD
 		//Going Down
 		//the "+ 1" is to skip checking the same space the piece it's on
 		for (int i = position[0] + 1; i < 8; i++) {
@@ -88,6 +127,8 @@ public class Rook extends Piece {
 		}
 		System.out.print("\n");
 		
+=======
+>>>>>>> 341b3af (Updated: Piece to have a method to handle space legality)
 		/*Calculation horizontal spaces*/
 		//Left 
 		for (int i = position[1] - 1; i > -1; i--) {
