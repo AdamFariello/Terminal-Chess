77e06f7225dd54d0ec850f35dff9f5bccccbd04f
adamfariello <adam.fariello@rutgers.edu>
2022-03-21 22:25:07 -0400

    Up to date with system code

--- src/pieces/Rook.java
+++ src/pieces/Rook.java
@@ -29,48 +29,6 @@ public class Rook extends Piece {
 		
 		/*Calculation vertical spaces*/
 		//Going Up
-		//the "-1" is to skip checking the same space the piece it's on
-<<<<<<< HEAD
-		/*
-		for (int i = position[0] - 1; i > -1; i--) {
-			System.out.printf("Calculating (%d,%d) ", i, position[1]);
-			if (board[i][position[1]].getPiece() == null) {
-				//Blank Space, free spot
-				int [] temp = {i, position[1]};
-				moves.add(rankFileConversion.ArraytoRankFile(temp));
-			} else if (board[i][position[1]].getPiece() != null && 
-					   board[i][position[1]].getPiece().getPieceName().charAt(0) != 
-					   this.getPieceName().charAt(0)) {
-				//Space is an enemy spot, can take, but can't go beyond
-				int [] temp = {i, position[1]};
-				moves.add(rankFileConversion.ArraytoRankFile(temp));
-				break;
-			} else {
-				//Friendly piece detected, can't move there or anywhere else
-				break;
-				test
-<<<<<<< HEAD
-			}
-		}
-		System.out.print("\n");
-		*/
-		for (int i = position[0] - 1; i > -1; i--) {
-			String temp = checkSpace(board, i, position[1]);
-			if (temp != null) {
-				if (Character.isUpperCase(temp.charAt(0)) == true) {
-					moves.add(temp.toLowerCase());
-					break;
-				} else {
-					moves.add(temp);
-				}
-			} else { 
-=======
-			}
-		}
-		System.out.print("\n");
-		*/
-=======
->>>>>>> a99e845 (go through)
 		for (int i = position[0] - 1; i > -1; i--) {
 			String temp = checkSpace(board, i, position[1]);
 			if (temp != null) {
@@ -103,37 +61,11 @@ public class Rook extends Piece {
 				break;
 			} else {
 				//Friendly piece detected, can't move there or anywhere else
->>>>>>> 341b3af (Updated: Piece to have a method to handle space legality)
 				break;
 			}
 		}
 		System.out.print("\n");
 		
-<<<<<<< HEAD
-		//Going Down
-		//the "+ 1" is to skip checking the same space the piece it's on
-		for (int i = position[0] + 1; i < 8; i++) {
-			System.out.printf("Calculating (%d,%d) ", i, position[1]);
-			if (board[i][position[1]].getPiece() == null) {
-				//Blank Space, free spot
-				int [] temp = {i, position[1]};
-				moves.add(rankFileConversion.ArraytoRankFile(temp));
-			} else if (board[i][position[1]].getPiece() != null && 
-					   board[i][position[1]].getPiece().getPieceName().charAt(0) != 
-					   this.getPieceName().charAt(0)) {
-				//Space is an enemy spot, can take, but can't go beyond
-				int [] temp = {i, position[1]};
-				moves.add(rankFileConversion.ArraytoRankFile(temp));
-				break;
-			} else {
-				//Friendly piece detected, can't move there or anywhere else
-				break;
-			}
-		}
-		System.out.print("\n");
-		
-=======
->>>>>>> 341b3af (Updated: Piece to have a method to handle space legality)
 		/*Calculation horizontal spaces*/
 		//Left 
 		for (int i = position[1] - 1; i > -1; i--) {
